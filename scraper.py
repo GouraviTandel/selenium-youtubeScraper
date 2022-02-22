f5e360ecbc441996e665fb2c213f0b4f5116bd86
gouravi <47203436+GouraviTandel@users.noreply.github.com>
2022-02-22 10:29:54 +0000

    Added Selenium Code

--- scraper.py
+++ scraper.py
@@ -1,20 +1,26 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 youtube_scraper_url="https://www.youtube.com/feed/trending"
 
-response=requests.get(youtube_scraper_url)
-print('Status Code:', response.status_code)
-with open('trending.html','w') as f:
-  f.write(response.text)
+def get_driver():
+  chrome_options = Options()
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  driver=webdriver.Chrome(options=chrome_options)
+  return driver
 
-with open('trending.html','r') as f:
-  html_source=f.read()
-doc=BeautifulSoup(html_source,'html.parser')
-print('title:',doc.title.text)
+if __name__=="__main__":
+  print('Creating Driver')
+  driver=get_driver()
 
-#Find all video Div 
+print("Fetching the page")
+driver.get(youtube_scraper_url)
 
-video_divs=doc.find_all('div',class_="style-scope ytd-video-renderer")
-print(video_divs)
+print('Get Video Divs')
+Video_div_class='ytd-video-renderer'
+video_divs=driver.find_elements_by_class_name(Video_div_class)
+print(f'found {len(video_divs)} Videos')
 
+print('Page Title using Selenium',driver.title)
