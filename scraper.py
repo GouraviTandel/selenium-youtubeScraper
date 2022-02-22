e0e00ea1978c00ece3353a947cd7ea27a0291d57
gouravi <47203436+GouraviTandel@users.noreply.github.com>
2022-02-22 07:57:06 +0000

    Download the Webpage as html using BeautifulSoup

--- scraper.py
+++ scraper.py
@@ -1,1 +1,20 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+
+youtube_scraper_url="https://www.youtube.com/feed/trending"
+
+response=requests.get(youtube_scraper_url)
+print('Status Code:', response.status_code)
+with open('trending.html','w') as f:
+  f.write(response.text)
+
+with open('trending.html','r') as f:
+  html_source=f.read()
+doc=BeautifulSoup(html_source,'html.parser')
+print('title:',doc.title.text)
+
+#Find all video Div 
+
+video_divs=doc.find_all('div',class_="style-scope ytd-video-renderer")
+print(video_divs)
+
