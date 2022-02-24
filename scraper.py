f3405966e2d4004449c21a6d95c324291f0b40f3
gouravi <47203436+GouraviTandel@users.noreply.github.com>
2022-02-24 10:42:58 +0000

    Update

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import time
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -17,7 +18,9 @@ def get_driver():
 def get_videos(driver):
   driver.get(youtube_scraper_url)
   Video_div_tag='ytd-video-renderer'
-  driver.implicitly_wait(20)
+  driver.implicitly_wait(30)
+  driver.execute_script("window.scrollTo(0, 10000);")
+  time.sleep(2)
   videos=driver.find_elements(By.TAG_NAME,Video_div_tag)
   return videos
 
@@ -36,6 +39,9 @@ def parse_video(video):
          'Thumbnail image src': thumbnail_imgtag,
          'Channel Name': Channel_name,
          'Description': description_text}
+
+def send_email():
+  pass
   
 if __name__=="__main__":
   print('Creating Driver')
@@ -45,10 +51,12 @@ if __name__=="__main__":
   print("Found:",len(videos))
 
   print("Parsing Top 10 Videos")
-  videosdata=[parse_video(i) for i in videos[:10]]
+  videosdata=[parse_video(i) for i in videos[:11]]
   #print(videosdata)
   print("save to CSV file")
   video_df=pd.DataFrame(videosdata)
   #print(video_df)
   video_df.to_csv('Trending_Video.csv')
+
+  print('Send an email with the results')
 
